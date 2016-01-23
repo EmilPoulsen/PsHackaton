6bd53f811d13d20fe31c459de9a3b88863795e7d
Emil Poulsen <emil-poulsen@hotmail.com>
2016-01-23 12:56:46 +0100

    test

---
+++ Hackaton_sandbox/isaksclass.cpp
@@ -0,0 +1,6 @@
+#include "isaksclass.h"
+
+IsaksClass::IsaksClass()
+{
+
+}
---
+++ Hackaton_sandbox/isaksclass.h
@@ -0,0 +1,15 @@
+#ifndef ISAKSCLASS_H
+#define ISAKSCLASS_H
+
+#include <string>
+#include "mainwindow.h"
+
+class IsaksClass
+{
+    int a;
+    std::string text;
+public:
+    IsaksClass();
+};
+
+#endif // ISAKSCLASS_H
